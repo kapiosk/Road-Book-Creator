6663ee04d5bc3e4df6c272566165d484ae947968
kapiosk <dkknd@hotmail.com>
2019-10-24 20:43:34 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const cacheName = 'rbc';
+// const cacheName = 'rbc';
 
-const staticAssets = [
-  'index.html'
-];
+// const staticAssets = [
+//   'index.html'
+// ];
 
-self.addEventListener('install', async function () {
-  const cache = await caches.open(cacheName);
+// self.addEventListener('install', async function () {
+  // const cache = await caches.open(cacheName);
   // cache.addAll(staticAssets);
-});
+// });
 
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
@@ -19,14 +19,16 @@ self.addEventListener('fetch', event => {
   // if (url.origin === location.origin) {
   // event.respondWith(cacheFirst(request));
   // } else {
+  if (request.url.indexOf('http') === 0) {
     event.respondWith(networkFirst(request));
+  }
   // }
 });
 
-async function cacheFirst(request) {
-  const cachedResponse = await caches.match(request);
-  return cachedResponse || fetch(request);
-}
+// async function cacheFirst(request) {
+//   const cachedResponse = await caches.match(request);
+//   return cachedResponse || fetch(request);
+// }
 
 async function networkFirst(request) {
   const dynamicCache = await caches.open('dynamicCache');
